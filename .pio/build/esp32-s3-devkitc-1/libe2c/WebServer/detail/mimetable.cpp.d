bcc63de14cfa8167066cf6eb1f92746bfea66be4
Bartek <bartek.sypki@gmail.com>
2026-03-04 17:06:22 +0100

    Initializing PIO

--- .pio/build/esp32-s3-devkitc-1/libe2c/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/libe2c/WebServer/detail/mimetable.cpp.o: \
- C:/Users/Bartek/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/Bartek/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/Bartek/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
--- .pio/build/esp32-s3-devkitc-1/libf73/BLE/BLEExceptions.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/libf73/BLE/BLEExceptions.cpp.o: \
- C:/Users/Bartek/.platformio/packages/framework-arduinoespressif32/libraries/BLE/src/BLEExceptions.cpp
